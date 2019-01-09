fea42d319576e5e799b5f914ea6334f85885dd2c
isaachambers <isaaceduardk@gmail.com>
2019-01-09 14:18:03 +0300

    Adding Application Tests

---
+++ test/UserControllerTest.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const usersController = require('../controllers/UserController')();
+
+describe('UsersController', function(){
+    describe('Creating Users', function(){
+        it('Should Save User into DB', function(){
+           
+        })
+    })
+})
